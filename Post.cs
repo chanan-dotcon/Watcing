9029865080e4d942849f4eb04c2f827a902f7cab
chanan <chanan916@gmail.com>
2020-06-21 20:26:21 +0300

    second commit

---
+++ Post.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProject.Classes.Exercises
+{
+    public class Post
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int TotalVotes { get; private set; }
+        private DateTime _cuurentTime;
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            _cuurentTime = DateTime.Now;
+        }
+
+        public void PrintPost()
+        {
+            Console.WriteLine($"Title: {Title}\n" +
+                $"Description: {Description}\n" +
+                $"Date and Time: {_cuurentTime}");
+        }
+
+        public int UpVote()
+        {
+            return TotalVotes += 1;
+        }
+
+        public int DownVote()
+        {
+            return TotalVotes -= 1;
+        }
+
+        public void CurrentVotes()
+        {
+            Console.WriteLine($"Votes: {TotalVotes}");
+        }
+
+        //Program
+        //var post = new Post("How to add vote", "I want to be know");
+        //post.PrintPost();
+        //post.UpVote();
+        //post.UpVote();
+        //post.UpVote();
+        //post.DownVote();
+        //post.DownVote();
+        //post.DownVote();
+        //post.DownVote();
+        //post.CurrentVotes();
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProject.Classes.Exercises
+{
+    public class StopWatch
+    {
+        private TimeSpan _total;
+        private DateTime _startWatch;
+        private DateTime _endWatch;
+
+        public void Strat()
+        {
+            if (_startWatch.Millisecond == 0)
+                _startWatch = DateTime.Now;
+            else
+                Console.WriteLine(new InvalidOperationException("Cannot be starting twice"));
+        }
+
+        public void Stop()
+        {
+            _endWatch = DateTime.Now;
+        }
+
+        public void DurationWatch()
+        {
+            _total = (_endWatch - _startWatch);
+            Console.WriteLine($"The total the time watching: " +
+                $"Hours: {_total.Hours}, Minutes: {_total.Minutes}," +
+                $" MilliSeconds: {_total.Milliseconds}");
+            //For reset 'DateTime'
+            _startWatch = new DateTime();
+        }
+
+        //Program
+        //var stop = new StopWatch();
+        //stop.Strat();
+        //stop.Strat();
+        //stop.Stop();
+        //stop.DurationWatch();
+        //stop.Strat();
+        //stop.Stop();
+        //stop.DurationWatch();
+    }
+}
